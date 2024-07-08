ad8593daa131b8cf4338fd17e1580da51882d156
Sab0001goat <sabakupreishvili07@gmail.com>
2024-07-08 17:07:39 +0400

    dasdadasd

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 console.log("saba");
 document.getElementById("hone").textContent = "kuuperaba";
-let age = 19
-let price = 90
-console.log(x);
+let age = 19;
+let price = 90;
+let student = 30;
+let nigga = 733;
+console.log(student);
+console.log(nigga);
+
+console.log(student + nigga);
+function favoriteAnimal(geka) {
+  return geka + " is my favorite animal!";
+}
+
+console.log(favoriteAnimal("red nigga"));
