16abd7b95193898b82ab0c965a3ab745130c0e7a
Sab0001goat <sabakupreishvili07@gmail.com>
2024-07-18 16:20:10 +0400

    svkjdckj;dskbjdsbjhdfjbsatannnnn

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-console.log("saba");
-document.getElementById("hone").textContent = "kuuperaba";
-let age = 19;
-let price = 90;
-let student = 30;
-let nigga = 733;
-console.log(student);
-console.log(nigga);
-
-console.log(student + nigga);
-function favoriteAnimal(geka) {
-  return geka + " is my favorite animal!";
+let answer = parseInt(prompt("pls writ =e thenumber you wannaniggbugg me as"));
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("niggbugg");
+  } else if (i % 3 === 0) {
+    console.log("nigg) ");
+  } else if (i % 5 === 0) {
+    console.log("bugg");
+  } else {
+    console.log(i);
+  }
 }
-
-console.log(favoriteAnimal("red nigga"));
